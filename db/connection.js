e70f31eec40e474959b3d59f5588fc06e798a4f0
Cipherr674 <71639314+Cipherr674@users.noreply.github.com>
2025-01-31 14:17:36 +0530

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-mongoose.connect('mongodb+srv://anupmanil10:5bACCJgShJVgqvwC@cluster0.cjnux.mongodb.net/EmployeeDatabase1?retryWrites=true&w=majority&appName=Cluster0').then(()=>{
+mongoose.connect('mongodb+srv://anupmanil10:1234@cluster0.cjnux.mongodb.net/EmployeeDatabase1?retryWrites=true&w=majority&appName=Cluster0').then(()=>{
     console.log('Connection established to DB');
 }).catch(()=>{
     console.log('Not connected');
